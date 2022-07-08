3dbbdb4ea69910a100a2de9a350a2ed8b2a3043a
ot-nemoto <nemoto@opentone.co.jp>
2022-07-08 04:58:24 +0000

    人口マップ人口構成_人口ピラミッド

--- src/resas_api/api/v1/population/composition.py
+++ src/resas_api/api/v1/population/composition.py
@@ -23,3 +23,27 @@ def per_year(**kwargs):
         headers=headers)
 
     return response.json()
+
+def pyramid(**kwargs):
+    pref_code = kwargs.get('prefCode')
+    city_code = kwargs.get('cityCode')
+    year_left = kwargs.get('yearLeft')
+    year_right = kwargs.get('yearRight')
+    add_area = list(filter(lambda x: x != '', kwargs.get('addArea', '').split(',')))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/composition/v1/pyramid/{pref_code}/{city_code}/{year_left}/{year_right}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code, year_left=year_left, year_right=year_right),
+        params=payload,
+        headers=headers)
+
+    return response.json()
