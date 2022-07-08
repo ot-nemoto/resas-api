a3867c376a9c3bc657c19b191b1d2681cc3b3c47
ot-nemoto <nemoto@opentone.co.jp>
2022-07-08 15:02:50 +0000

    人口マップ_人口増減_人口増減率

---
+++ src/resas_api/api/v1/population/sum.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+def per_year(**kwargs):
+    pref_code = kwargs.get('prefCode')
+    city_code = kwargs.get('cityCode')
+    add_area = list(filter(lambda x: x != '', kwargs.get('addArea', '').split(',')))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/sum/v1/forLineBar/{pref_code}/{city_code}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code),
+        params=payload,
+        headers=headers)
+
+    return response.json()
