7412b33d390a4c1c0d1ad621aff3c3c751e53ff6
ot-nemoto <nemoto@opentone.co.jp>
2022-07-11 00:32:55 +0000

    出生数・死亡数／転入数・転出数

--- src/resas_api/api/v1/population/sum.py
+++ src/resas_api/api/v1/population/sum.py
@@ -23,3 +23,25 @@ def per_year(**kwargs):
         headers=headers)
 
     return response.json()
+
+def estimate(**kwargs):
+    pref_code = kwargs.get('prefCode')
+    city_code = kwargs.get('cityCode')
+    add_area = list(filter(lambda x: x != '', kwargs.get('addArea', '').split(',')))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/sum/v1/sumLine/{pref_code}/{city_code}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code),
+        params=payload,
+        headers=headers)
+
+    return response.json()
