773c3bbe1bb99dc3cbdb9a23b82d8a1cd867685b
ot-nemoto <nemoto@opentone.co.jp>
2022-07-17 08:39:25 +0000

    産業構造マップ_全産業_地域別特化係数

--- src/resas_api/api/v1/industry/power.py
+++ src/resas_api/api/v1/industry/power.py
@@ -22,3 +22,31 @@ def for_industry(**kwargs):
     logging.debug('RESAS: {}'.format(response.url))
 
     return response.json()
+
+def for_area(**kwargs):
+    year = kwargs.get('year')
+    pref_code = kwargs.get('prefCode')
+    area_type = kwargs.get('areaType')
+    disp_type = kwargs.get('dispType')
+    sic_code = kwargs.get('sicCode')
+    simc_code = kwargs.get('simcCode')
+    add_industry = list(filter(lambda x: x != '', kwargs.get('addIndustry', '').split(',')))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/industry/power/v1/forAreaBar/{year}/{pref_code}/{area_type}/{disp_type}/{sic_code}/{simc_code}'
+
+    payload = {}
+    if len(add_industry):
+        payload['addIndustry'] = add_industry
+
+    response = requests.get(
+        url.format(year=year, pref_code=pref_code, area_type=area_type, disp_type=disp_type, sic_code=sic_code, simc_code=simc_code),
+        params=payload,
+        headers=headers)
+
+    logging.debug('RESAS: {}'.format(response.url))
+
+    return response.json()
