75814b5e65829083efe89f8f2fb204d631e3f818
ot-nemoto <nemoto@opentone.co.jp>
2022-07-23 13:54:58 +0000

    openapiの配置を変更

--- src/resas_api/app.py
+++ src/resas_api/app.py
@@ -24,8 +24,8 @@ dictConfig({
 })
 
 options = {"swagger_ui": True}
-app = connexion.FlaskApp(__name__, specification_dir='openapi/', options=options)
-app.add_api('resas_api.yaml')
+app = connexion.FlaskApp(__name__, specification_dir='.', options=options)
+app.add_api('openapi.yaml')
 app.app.config['JSON_AS_ASCII'] = False
 app.app.config['JSON_SORT_KEYS'] = False
 
