fc85d45aad383647dd7620e451e86b8179357af7
ot-nemoto <nemoto@opentone.co.jp>
2022-09-17 14:19:46 +0000

    人口マップ / 人口増減
    - 人口増減
    - 出生数・死亡数 / 転入数・転出数
    - 自然増減・社会増減の推移

--- src/resas_api/api/v1/population/sum.py
+++ src/resas_api/api/v1/population/sum.py
@@ -3,6 +3,108 @@
 import requests
 import logging
 
+def for_line_bar(**kwargs):
+    body = kwargs.get('body')
+    if ('pref_code' in body):
+        pref_code = int(body['pref_code'][0])
+        city_code = '-'
+        add_area = list(map(lambda x: '{}_'.format(int(x)), body['pref_code'][1:]))
+
+    if ('city_code' in body):
+        pref_code = int(body['city_code'][0][:2])
+        city_code = body['city_code'][0]
+        add_area = list(map(lambda x: '{}_{}'.format(int(x[:2]), x), body['city_code'][1:]))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/sum/v1/forLineBar/{pref_code}/{city_code}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code),
+        params=payload,
+        headers=headers)
+
+    logging.debug('RESAS: {}'.format(response.url))
+
+    return {
+        'params': body,
+        'result': response.json().get('result').get('bar'),
+    }
+
+def sum_line(**kwargs):
+    body = kwargs.get('body')
+    if ('pref_code' in body):
+        pref_code = int(body['pref_code'][0])
+        city_code = '-'
+        add_area = list(map(lambda x: '{}_'.format(int(x)), body['pref_code'][1:]))
+
+    if ('city_code' in body):
+        pref_code = int(body['city_code'][0][:2])
+        city_code = body['city_code'][0]
+        add_area = list(map(lambda x: '{}_{}'.format(int(x[:2]), x), body['city_code'][1:]))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/sum/v1/sumLine/{pref_code}/{city_code}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code),
+        #params=payload,
+        headers=headers)
+
+    logging.debug('RESAS: {}'.format(response.url))
+
+    return {
+        'params': body,
+        'result': response.json().get('result'),
+    }
+
+def transition_bar_line(**kwargs):
+    body = kwargs.get('body')
+    if ('pref_code' in body):
+        pref_code = int(body['pref_code'][0])
+        city_code = '-'
+        add_area = list(map(lambda x: '{}_'.format(int(x)), body['pref_code'][1:]))
+
+    if ('city_code' in body):
+        pref_code = int(body['city_code'][0][:2])
+        city_code = body['city_code'][0]
+        add_area = list(map(lambda x: '{}_{}'.format(int(x[:2]), x), body['city_code'][1:]))
+
+    headers = {
+        'Referer': 'https://resas.go.jp/',
+    }
+
+    url = 'https://resas.go.jp/api/population/sum/v1/transitionBarLine/{pref_code}/{city_code}'
+
+    payload = {}
+    if len(add_area):
+        payload['addArea'] = add_area
+
+    response = requests.get(
+        url.format(pref_code=pref_code, city_code=city_code),
+        params=payload,
+        headers=headers)
+
+    logging.debug('RESAS: {}'.format(response.url))
+
+    return {
+        'params': body,
+        'result': response.json().get('result'),
+    }
+
 def per_year(**kwargs):
     pref_code = kwargs.get('prefCode')
     city_code = kwargs.get('cityCode')
